879cc2d7aa0ab583bef9153332540524c6e22f85
unknown <matthewharre@gmail.com>
2022-03-21 13:22:02 -0700

    added userItem, userList, Avatar, and Card components with subsequent css files, added a fake user array to display 1 user for testing purposes, added more organizational structured folders

--- main-project/src/user/pages/Users.js
+++ main-project/src/user/pages/Users.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Bob Smith',
+            image: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+            places: 3
+        }
+    ];
+
     return (
-        <h2> Users works!! </h2>
+        <UsersList
+            items={USERS}
+        />
     )
 };
 

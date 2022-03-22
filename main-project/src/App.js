2ebf2681e63db9b24364849bb061b64141b62db0
unknown <matthewharre@gmail.com>
2022-03-22 13:36:13 -0700

    added placeItem, placeList component, and a fake array in UserPlaces just for displaying purposes

--- main-project/src/App.js
+++ main-project/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
+import UserPlaces from './places/pages/UserPlaces';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
@@ -13,6 +14,9 @@ function App() {
           <Switch>
             <Route path ='/' exact>
               <Users />
+            </Route>
+            <Route path='/:userId/places' exact>
+              <UserPlaces />
             </Route>
             <Route path ='/places/new' exact>
               <NewPlace />

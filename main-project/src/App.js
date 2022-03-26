1e64f5622aeff18d5a8cfa6c9a9c0776c52304f9
unknown <matthewharre@gmail.com>
2022-03-25 20:08:11 -0700

    added new route, added a fake array for displaying

--- main-project/src/App.js
+++ main-project/src/App.js
@@ -5,6 +5,7 @@ import UserPlaces from './places/pages/UserPlaces';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 function App() {
   return (
@@ -12,14 +13,17 @@ function App() {
       <MainNavigation />
         <main>
           <Switch>
-            <Route path ='/' exact>
+            <Route path='/' exact>
               <Users />
             </Route>
             <Route path='/:userId/places' exact>
               <UserPlaces />
             </Route>
-            <Route path ='/places/new' exact>
+            <Route path='/places/new' exact>
               <NewPlace />
+            </Route>
+            <Route path='/places/:placeId'> 
+              <UpdatePlace />
             </Route>
             <Redirect to='/' />
           </Switch>
@@ -29,3 +33,7 @@ function App() {
 }
 
 export default App;
+
+// order of routes in /places/new and /places/:placeId matters
+// if /:placeId was first react would still interpret /places/new the same as /places/:placeId because of the dynamic url
+// /places/new which renders NewPlace would never be matched at that point so the order is important

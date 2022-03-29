46e02e7c799308f28949a8e89868b991d29bfc8d
unknown <matthewharre@gmail.com>
2022-03-29 10:44:46 -0700

    added nodejs example and some basic starting files for express

---
+++ misc-practice/express/app.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log('INCOMING REQUEST');
+    console.log(req.method, req.url); //gives us whatever method was used and url 
+
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('end', () => {
+            //console.log(body)
+            const userName = body.split('=')[1]; // returns 2 arrays, before / after split
+            res.end(`${userName}`)
+        });
+
+        req.on('data', (chunk) => { // we get data in chunks, not all at once
+            body += chunk;
+        });
+    } else {
+        res.setHeader('Content-Type', 'text/html')
+        res.end('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
+    }
+});
+
+server.listen(5000);

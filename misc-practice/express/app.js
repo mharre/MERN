a350a582ab0b99682f0edf7a6fee0e92084265d3
unknown <matthewharre@gmail.com>
2022-03-29 13:04:12 -0700

    added basic express example

--- misc-practice/express/app.js
+++ misc-practice/express/app.js
@@ -1,23 +1,30 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log('INCOMING REQUEST');
-    console.log(req.method, req.url); //gives us whatever method was used and url 
+const express = require('express');
+const bodyParser = require('body-parser'); 
 
-    if (req.method === 'POST') {
-        let body = '';
-        req.on('end', () => {
-            //console.log(body)
-            const userName = body.split('=')[1]; // returns 2 arrays, before / after split
-            res.end(`${userName}`)
-        });
+const app = express();
 
-        req.on('data', (chunk) => { // we get data in chunks, not all at once
-            body += chunk;
-        });
-    } else {
-        res.setHeader('Content-Type', 'text/html')
-        res.end('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
-    }
+//app.use((req, res, next) => {
+//    let body = '';
+//    req.on('end', () => {
+//        const userName = body.split('=')[1]
+//        if (userName) {
+//            req.body = {name: userName}
+//        }
+//        next(); // only goes to next if we are done parsing incoming data
+//    })
+//    req.on('data', (chunk) => {
+//        body += chunk;
+//    })
+//});
+
+app.use(bodyParser.urlencoded({ extended: false })); //parse ALL incoming request and try to extract all data possible, also calls next for us
+
+app.post('/user', (req, res, next) => { // 2 params, first is filtering for path, second is func
+    res.send(`<h1>${req.body.username}</h1>`); //available based on the name="..."
 });
 
-server.listen(5000);
+app.get('/', (req, res, next) => {
+    res.send('<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
+});
+
+app.listen(5000);

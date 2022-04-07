2150357bb5d56060ff84cc57b05a8271ff8d68d8
unknown <matthewharre@gmail.com>
2022-04-07 10:52:55 -0700

    cleaned up custom hook, cleaned up code in auth.js to use the custom hook instead of the repeated request code

--- main-project/frontend/src/shared/hooks/http-hook.js
+++ main-project/frontend/src/shared/hooks/http-hook.js
@@ -1,48 +1,58 @@
 import { useState, useCallback, useRef, useEffect } from 'react';
 
 export const useHttpClient = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
-    const activeHttpRequest = useRef([]); // stores data across rerender cycles, w/o this there is an issue when changing the page if there are active requests still going
+  const activeHttpRequests = useRef([]); // stores data across rerender cycles, w/o this there is an issue when changing the page if there are active requests still going
 
-    const sendRequest = useCallback( async (url, method='GET', body=null, headers={}) => {
-        setIsLoading(true);
-        const httpAbortCtrl = new AbortController();
-        activeHttpRequest.current.push(httpAbortCtrl);
+  const sendRequest = useCallback(
+    async (url, method = 'GET', body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
 
-        try {
-            const response = await fetch(url, {
-                method,
-                body,
-                headers,
-                signal: httpAbortCtrl.signal //basically links the abort controller to this specific request, we can now use it to cancel request
-            });
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal //basically links the abort controller to this specific request, we can now use it to cancel request
+        });
 
-            const responseData = response.json();
+        const responseData = await response.json();
 
-            if (!response.ok) {
-                throw new Error(responseData.message);
-            }
-            //return data for our component to use
-            return responseData
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          reqCtrl => reqCtrl !== httpAbortCtrl
+        );
 
-        } catch(err) {
-            setError(err.message);
+        if (!response.ok) {
+          throw new Error(responseData.message);
         }
-        setIsLoading(false)
-    }, []);
 
-    const clearError = () => {
-        setError(null);
+        setIsLoading(false);
+        
+        //return data for our component to use
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => { //using this for clean up code to run when component unmounts. the returned function is executed as a clean up func
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort()); //aborts request to which it is linked with
     };
-
-    useEffect(() => { //using this for clean up code to run when component unmounts. the returned function is executed as a clean up func
-        return () => {
-            activeHttpRequest.current.forEach(abortCtrl => abortCtrl.abort()) //aborts request to which it is linked with
-        };
-    }, [])
-
-    //return sendRequest func, isloading, and error so we can also use
-    return { isLoading, error, sendRequest, clearError }
+  }, []);
+    
+  //return sendRequest func, isloading, and error so we can also use
+  return { isLoading, error, sendRequest, clearError };
 };
--- main-project/frontend/src/user/pages/Auth.js
+++ main-project/frontend/src/user/pages/Auth.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import { useForm } from '../../shared/hooks/form-hook';
 import {VALIDATOR_MINLENGTH, VALIDATOR_EMAIL, VALIDATOR_REQUIRE} from '../../shared/utils/validators';
@@ -14,8 +15,7 @@ const Auth = () => {
     const auth = useContext(AuthContext);
 
     const [isLoginMode, setIsLoginMode] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
+    const {isLoading, error, sendRequest, clearError } = useHttpClient();
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -50,74 +50,43 @@ const Auth = () => {
     const authSubmitHandler = async (event) => {
         event.preventDefault();
 
-        setIsLoading(true);
-
         if (isLoginMode) {
             try {
-                const response = await fetch('http://localhost:5000/api/users/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json' 
-                    },
-                    body: JSON.stringify({
+                await sendRequest( 
+                    'http://localhost:5000/api/users/login',
+                    'POST',
+                    JSON.stringify({
                         email: formState.inputs.email.value,
                         password: formState.inputs.password.value, 
-                    })
-                });
-
-                const responseData = await response.json(); 
-                if (!response.ok) {
-                    throw new Error(responseData.message); 
-                }
-                //console.log(responseData)
-
-                setIsLoading(false);
+                    }),
+                    {
+                        'Content-Type': 'application/json' 
+                    }, 
+                );
                 auth.login(); 
-            } catch (err) {
-                console.log(err);
-
-                setIsLoading(false);
-                setError(err.message || 'Something went wrong, please try again');
-            }
-
+            } catch (err) {} //we can leave this empty for this specific request because we are handling everything inside of our custom hook
         } else {
             try {
-                const response = await fetch('http://localhost:5000/api/users/signup', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json' // so our BE knows it's JSON data incoming
-                    },
-                    body: JSON.stringify({
+                await sendRequest(
+                    'http://localhost:5000/api/users/signup',
+                    'POST',
+                    JSON.stringify({
                         name: formState.inputs.name.value,
                         email: formState.inputs.email.value,
                         password: formState.inputs.password.value, //these are the fields we are expecting on the BE
-                    })
-                });
-
-                const responseData = await response.json(); // our BE sends back the created user
-                if (!response.ok) {
-                    throw new Error(responseData.message); // since we throw error here we will make it into catch block, fetch works that even if 400/500 still technically no "error"
-                }
-                //console.log(responseData)
-
-                setIsLoading(false);
+                    }),
+                    {
+                        'Content-Type': 'application/json' // so our BE knows it's JSON data incoming
+                    },
+                );
                 auth.login(); // only runs after our response 
-            } catch (err) {
-                console.log(err);
-
-                setIsLoading(false);
-                setError(err.message || 'Something went wrong, please try again');
-            }
+            } catch (err) {} // leave blank same as above
         }
-    };
-
-    const errorHandler = () => {
-        setError(null);
     };
 
     return (
         <React.Fragment>
-            <ErrorModal error={error} onClear={errorHandler} />
+            <ErrorModal error={error} onClear={clearError} />
             <Card className='authentication'>
                 {isLoading && <LoadingSpinner asOverlay /> }
                 <h2>Login Required</h2>

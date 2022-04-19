0ce6a801a8d78185df0bb5bf859e0e164fc934cf
unknown <matthewharre@gmail.com>
2022-04-19 15:18:11 -0700

    install 2 new packages: bcrypt/jsonwebtoken, added logic to hash/salt passwords on signup, added logic to generate tokens in signup/login

--- main-project/backend/controllers/users-controller.js
+++ main-project/backend/controllers/users-controller.js
@@ -1,5 +1,7 @@
 //const uuid = require('uuid');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -42,11 +44,19 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12); // 12 = standard, can't be reverse engineered and doesn't take hours to generate
+    } catch(err) {
+        const error = new HttpError('Could not create user, please try again', 500)
+        return next(error);
+    }
+
     const createdUser = new User({
         name,
         email,
         image: req.file.path,
-        password,
+        password: hashedPassword,
         places: []
     });
 
@@ -63,7 +73,19 @@ const signup = async (req, res, next) => {
         return next(error)
     }
 
-    res.status(201).json({user: createdUser.toObject({ getters:true })});
+    let token; //1st arg = payload, data we want to encode into token, 2nd=private string only server knows, 3rd=configuration obj
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' } //best practice is to let expire
+        ); 
+    } catch(err) {
+        const error = new HttpError('Signup failed', 500);
+        return next(error)
+    }
+
+    res.status(201).json({ userId: createdUser.id, email:createdUser.email, token: token });
 
 };
 
@@ -78,10 +100,36 @@ const loginUser = async (req, res, next) => {
         return next(error);
     }
 
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
         const error = new HttpError('Invalid credentials, could not log you in', 401)
         return next(error);
     }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch(err) {
+        const error = new HttpError('Could not log you in, please check your credentials.', 500);
+        return next(error);
+    }
+
+    if (!isValidPassword) {
+        const error = new HttpError('Invalid credentials, could not log you in', 401)
+        return next(error);
+    }
+
+    let token; 
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share', //very important to use the same string as above, if it is different we will generate a different token
+            { expiresIn: '1h' } 
+        ); 
+    } catch(err) {
+        const error = new HttpError('Logging in failed', 500);
+        return next(error)
+    }
+
 
     res.status(200).json({
         message: 'Logged in!', 
